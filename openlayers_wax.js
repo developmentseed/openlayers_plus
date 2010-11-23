eee81a21aaf9645e285256dca989d8ba823b9b50
Young Hahn <young@developmentseed.org>
2010-11-22 23:30:48 -0500

    Add OL wax.

---
+++ openlayers_wax.js
@@ -0,0 +1,79 @@
+/**
+ * OpenLayers Wax machinery.
+ *
+ * Requirements:
+ *
+ * - OpenLayers
+ * - Underscore.js
+ */
+
+var OpenLayersWax = {
+    /**
+     * Get a function by string name without using eval
+     * @return {Function} the requested function.
+     */
+    get_function: function(head) {
+          return _.reduce(head.split('.'), function(f, cl) {
+            return f[cl];
+          }, window);
+    },
+
+    /**
+     * Instantiate JSON objects
+     * @param {Object} a json object.
+     * @return {Object} an instantiated OpenLayers or another type object.
+     */
+    reify: function(json_object) {
+        try {
+            if (json_object.hasOwnProperty('_type')) {
+                var fn = OpenLayersWax.get_function(json_object._type);
+                if (fn) {
+                    var obj = new fn();
+                    var waxed = OpenLayersWax.reify(json_object._value);
+                    fn.apply(obj, waxed);
+                    return obj;
+                }
+                else {
+                    console && console.log(json_object._type + ' missing');
+                    return [];
+                }
+            }
+            else if (_.isString(json_object) ||
+                    _.isNumber(json_object) || _.isBoolean(json_object)) {
+                return json_object;
+            }
+            else {
+                for (var i in json_object) {
+                    if (json_object.hasOwnProperty(i)) {
+                        json_object[i] = OpenLayersWax.reify(json_object[i]);
+                    }
+                }
+                return json_object;
+            }
+        } catch (e) {
+            console.log('Problem with: ' + JSON.stringify(json_object));
+            console.log(e);
+        }
+    },
+
+    /**
+     * Instantiate a map on an element
+     * @param {element} element to instantiate a map on.
+     */
+    bind: function() {
+        var element = _(arguments).last();
+        $.getJSON($(element).attr('src'), function(data) {
+            data.map && $(element).data('map',
+                new OpenLayers.Map(element, OpenLayersWax.reify(data.map)));
+            var degrees = new OpenLayers.LonLat(0, 0);
+            degrees.transform(
+                new OpenLayers.Projection('EPSG:4326'),
+                new OpenLayers.Projection('EPSG:900913'));
+            $(element).data('map').setCenter(degrees, 1);
+            data.externals && $(element).data(
+                'externals',
+                OpenLayersWax.reify(data.externals));
+            $(element).trigger('openlayersWaxFinished');
+        });
+    }
+};
